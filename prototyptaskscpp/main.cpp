b7217d1c305b1c2a27ebd390c1f0e728617d9da8
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-05 15:08:17 +0100

    main-Methode mit Parametern

--- prototyptaskscpp/main.cpp
+++ prototyptaskscpp/main.cpp
@@ -1,7 +1,36 @@
 
 #include "ebloads.cpp"
+#include <string.h>
 
-int main() {
-    run_dadd(1);
+int main(int argc,char *argv[]) {
+
+    int i;
+    printf("Anzahl Argumente: %d\n",argc);
+    for(i=0;i<argc;i++)
+    {
+        printf("%d. Argument: %s\n",i, argv[i]);
+    }
+
+    if (argc==2) {
+        printf("Es wurden zwei Argumente uebergeben, das 1. Element wird ausgewertet!\n");
+        if (strcmp(argv[1], "dadd") == 0)
+        {
+            printf("run_dadd");
+            run_dadd(1);
+        }
+        else if (strcmp(argv[1], "m4x4smul_SIMD") == 0)
+        {
+            printf("run_m4x4smul_SIMD");
+            run_m4x4smul_SIMD(1);
+        }
+        else /* default: */
+        {
+            printf("Parameter was [%s]", argv[1]);
+        }
+
+    } else {
+        printf("enter parameter e. g. dadd");
+    }
+
     return 0;
 }
