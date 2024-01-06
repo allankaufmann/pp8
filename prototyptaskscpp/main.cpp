28c45d4d0c23ad431e2494f95ae8b1c7d0504e43
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-06 12:29:58 +0100

    weitere tasks

--- prototyptaskscpp/main.cpp
+++ prototyptaskscpp/main.cpp
@@ -2,7 +2,21 @@
 #include "ebloads.cpp"
 #include <string.h>
 
-int main(int argc,char *argv[]) {
+static const char *const dadd = "dadd";
+
+static const char *const m4x4smul_SIMD = "m4x4smul_SIMD";
+
+static const char *const v1x4smul_SIMD = "v1x4smul_SIMD";
+
+static const char *const dmul64_SIMD = "dmul64_SIMD";
+
+static const char *const smul32_SIMD = "smul32_SIMD";
+
+static const char *const ssub32_SIMD = "ssub32_SIMD";
+
+
+
+int main(int argc, char *argv[]) {
 
     int i;
     printf("Anzahl Argumente: %d\n",argc);
@@ -13,24 +27,39 @@ int main(int argc,char *argv[]) {
 
     if (argc==2) {
         printf("Es wurden zwei Argumente uebergeben, das 1. Element wird ausgewertet!\n");
-        if (strcmp(argv[1], "dadd") == 0)
+        char* taskname = argv[1];
+        if (strcmp(argv[1], dadd) == 0)
         {
-            printf("run_dadd ");
             run_dadd(1);
-            printf("wurde ausgeführt\n");
+            printf("run_%s wurde ausgefuehrt", dadd);
         }
-        else if (strcmp(argv[1], "m4x4smul_SIMD") == 0)
+        else if (strcmp(taskname, m4x4smul_SIMD) == 0)
         {
-            printf("run_m4x4smul_SIMD ");
             run_m4x4smul_SIMD(1);
-            printf("wurde ausgeführt\n");
+            printf("run_%s wurde ausgefuehrt", m4x4smul_SIMD);
         }
-        else if (strcmp(argv[1], "v1x4smul_SIMD") == 0)
+        else if (strcmp(taskname, v1x4smul_SIMD) == 0)
         {
-            printf("run_v1x4smul_SIMD");
             run_v1x4smul_SIMD(1);
-            printf("wurde ausgeführt\n");
+            printf("run_%s wurde ausgefuehrt", v1x4smul_SIMD);
         }
+        else if (strcmp(taskname, dmul64_SIMD) == 0)
+        {
+            run_dmul64_SIMD(1);
+            printf("run_%s wurde ausgefuehrt", dmul64_SIMD);
+        }
+        else if (strcmp(taskname, smul32_SIMD) == 0)
+        {
+            run_smul32_SIMD(1);
+            printf("run_%s wurde ausgefuehrt", smul32_SIMD);
+        }
+        else if (strcmp(taskname, ssub32_SIMD) == 0)
+        {
+            run_ssub32_SIMD(1);
+            printf("run_%s wurde ausgefuehrt", ssub32_SIMD);
+        }
+
+
 
         else /* default: */
         {
