cda4f85210b095942b72049bf2c3b08b70072272
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-07 15:07:05 +0100

    update init

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ make
 cd ..
 git -C GDBInstructionScanner pull || git clone https://gitlab.abo.fi/sholmbac/GDBInstructionScanner.git
 cp GDBInstructionScanner/ci.py tests/ci.py
+cp GDBInstructionScanner/ci.py prototyptaskscpp/ci.py
 cp GDBInstructionScanner/test.c tests/test.c
