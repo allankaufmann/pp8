c7df04cf350fe7c7c8d97cb251eb43da12155585
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-11 18:15:43 +0100

    uarch-configure hinzugefügt

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ cp GDBInstructionScanner/ci.py tests/ci.py
 cp GDBInstructionScanner/ci.py prototyptaskscpp/ci.py
 cp GDBInstructionScanner/ci.py sampleapp/ci.py
 cp GDBInstructionScanner/test.c tests/test.c
+git -C uarch-configure pull || git clone https://github.com/deater/uarch-configure.git
+
+
