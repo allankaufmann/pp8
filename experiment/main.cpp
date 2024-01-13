ee5b0f88c5cab0adb10d3f7bce8c54bef8abd27a
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-13 15:15:39 +0100

    Dauer wird geloggt

--- experiment/main.cpp
+++ experiment/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <thread>
+#include <ctime>
+#include <chrono>
+
+void doSomething() {
+    system("./test.sh");
+}
+
+// https://stackoverflow.com/questions/19555121/how-to-get-current-timestamp-in-milliseconds-since-1970-just-the-way-java-gets
+uint64_t timeSinceEpochMillisec() {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+void logTime() {
+    std::time_t result = std::time(nullptr);
+    std::cout << std::ctime(&result);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    system("./test.sh");
+    logTime();
+    uint64_t begin = timeSinceEpochMillisec();
+
+    std::cout << "Thread wird gestartet\n";
+    std::thread t1(doSomething);
+    t1.join();
+
+    uint64_t end = timeSinceEpochMillisec();
+    uint64_t dauer = end - begin;
+
+    std::cout << "Dauer: " << dauer << " MS" << std::endl;
+    logTime();
     return 0;
 }
--- experiment/test.sh
+++ experiment/test.sh
@@ -3,6 +3,6 @@
 cd ..
 echo "$PWD"
 cd epEBench/bin/Release
-./epebench -m vidplay -t 1 -a 1 -n 1
+./epebench -m vidplay -t 3 -a 1 -n 1
 mv epebench_loadlog.txt vidplay.log
 cd ../../..
