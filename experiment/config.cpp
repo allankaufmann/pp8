e70ded28c0085962e1023c1298511963d3fa3feb
Allan Kaufmann <allan.kaufmann@studium.fernuni-hagen.de>
2024-01-27 16:21:37 +0100

    config als eigene source

---
+++ experiment/config.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+//#include <thread>
+//#include <ctime>
+//#include <chrono>
+#include <string.h> //strtok
+#include <sys/stat.h> //mkdir
+//#include <sys/types.h>
+//#include <dirent.h>
+//#include "measure.cpp"
+//#include <ctype.h>
+
+const char* config_filename = "experiment.ini";
+
+void writeGenScript(char* task) {
+    FILE* filePointerScript;
+    //char* filename = (char*) calloc(40, sizeof(char));
+    char* filename = (char*) malloc(sizeof(char) * (strlen(task)) + 10);
+    sprintf(filename,
+            "%s%s%s",
+            "gen/"
+            "run",
+            task,
+            ".sh");
+
+    filePointerScript = fopen(filename, "w");
+
+
+    fprintf(filePointerScript, "%s", "#!/bin/bash\n");
+    fprintf(filePointerScript, "%s", "cd ..\n");
+    fprintf(filePointerScript, "%s", "cd epEBench/bin/Release\n");
+    fprintf(filePointerScript, "%s%s%s", "./epebench -m ", task, " -t 1 -a 1 -n 1\n");
+    fprintf(filePointerScript, "%s%s%s", "mv epebench_loadlog.txt epebench_", task, ".log\n");
+    fprintf(filePointerScript, "%s", "cd ../../..\n");
+
+
+    fclose(filePointerScript);
+    chmod(filename, 0777);
+    free(filename);
+}
+
+void readConfigFile() {
+    mkdir("gen", 0777);
+    FILE* filePointer;
+    filePointer = fopen(config_filename, "r");
+    int bufferLength = 255;
+    char buffer[bufferLength];
+
+    char* task;
+
+    while(fgets(buffer, bufferLength, filePointer)) {
+        task = strtok(buffer, "=");
+        writeGenScript(task);
+    }
+
+    fclose(filePointer);
+    printf("%s", "Skripte zum Ausführen der prototypischen Tasks wurden im Ordner 'gen' erstellt!");
+}
+
--- experiment/main.cpp
+++ experiment/main.cpp
@@ -7,10 +7,11 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include "measure.cpp"
+#include "config.cpp"
 #include <ctype.h>
 const char* script_Sample_Application = "./scripts/runSampleApplication.sh";
 const char* script_testRapl = "./scripts/testRaplRead.sh";
-const char* config_filename = "experiment.ini";
+
 
 FILE* logfile;
 
@@ -155,50 +156,9 @@ void runSomething() {
     system("./scripts/runDadd.sh");
 }
 
-void writeGenScript(char* task) {
-    FILE* filePointerScript;
-    //char* filename = (char*) calloc(40, sizeof(char));
-    char* filename = (char*) malloc(sizeof(char) * (strlen(task)) + 10);
-    sprintf(filename,
-            "%s%s%s",
-            "gen/"
-            "run",
-            task,
-            ".sh");
-
-    filePointerScript = fopen(filename, "w");
 
 
-    fprintf(filePointerScript, "%s", "#!/bin/bash\n");
-    fprintf(filePointerScript, "%s", "cd ..\n");
-    fprintf(filePointerScript, "%s", "cd epEBench/bin/Release\n");
-    fprintf(filePointerScript, "%s%s%s", "./epebench -m ", task, " -t 1 -a 1 -n 1\n");
-    fprintf(filePointerScript, "%s%s%s", "mv epebench_loadlog.txt epebench_", task, ".log\n");
-    fprintf(filePointerScript, "%s", "cd ../../..\n");
 
-
-    fclose(filePointerScript);
-    chmod(filename, 0777);
-    free(filename);
-}
-
-void readConfigFile() {
-    mkdir("gen", 0777);
-    FILE* filePointer;
-    filePointer = fopen(config_filename, "r");
-    int bufferLength = 255;
-    char buffer[bufferLength];
-
-    char* task;
-
-    while(fgets(buffer, bufferLength, filePointer)) {
-        task = strtok(buffer, "=");
-        writeGenScript(task);
-    }
-
-    fclose(filePointer);
-    printf("%s", "Skripte zum Ausführen der prototypischen Tasks wurden im Ordner 'gen' erstellt!");
-}
 
 void runAllGenScripts() {
     DIR *dir;
@@ -252,10 +212,6 @@ int main(int argc, char *argv[]) {
     }
 
 
-
-    //hallowelt();
-    //
-    //runAllGenScripts();
     //testrapl();
     //testThreadWithRapl();
     //measureSampleApplication(script_Sample_Application);
